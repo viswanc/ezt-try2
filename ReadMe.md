127d4d5b8c565ac7dee16f3a146252b407384142
Viswanath Chidambaram <viswanc@thoughtworks.com>
2018-03-28 23:39:48 +0530

    Ran the HTTP service using config.

--- ReadMe.md
+++ ReadMe.md
@@ -14,3 +14,5 @@ Log
 * 180328
 
   * 2258  Initiated the repo with a simple working implementation of the http services.
+  * 2322  Ran a simple HTTP service.
+  * 2338  Ran the HTTP service using config.
---
+++ src/http/service/build.sh
@@ -0,0 +1,7 @@
+cd $(dirname "$0")
+
+env GOOS=linux GOARCH=amd64 go build -o bin/grpc-greeter server/main.go
+
+docker build -t gcr.io/api-project-483163119575/http-service .
+
+gcloud docker -- push gcr.io/api-project-483163119575/http-service
